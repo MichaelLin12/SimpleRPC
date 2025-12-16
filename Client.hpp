ab3ed3017a6cd35056c1d875a559fb5870a552c9
Michael Lin <michael.lin2032@gmail.com>
2025-12-15 23:13:02 -0500

    initial outline of Client object

---
+++ Client.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#include <string>
+
+class Client{
+public:
+    Client();
+
+
+    // Function templates (including template member functions) must be defined 
+    // in the header, not just declared.
+    template<typename Return, typename... Args>
+    Return call(std::string functionName, Args ... args){
+        if constexpr (std::is_void_v<Return>) {
+            // do work
+            return;
+        } else {
+            // do work
+            return result;
+        }
+    }
+};
