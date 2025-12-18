5b422a1f57b9e61abee83dd77994ee9469f631a4
Michael Lin <michael.lin2032@gmail.com>
2025-12-17 22:31:01 -0500

    build outline of serialier

---
+++ include/Codec/Serializer.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include "Msg/Message.hpp"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+/*
+* https://insujang.github.io/2022-03-12/implementing-basic-rpc-with-c-20/
+* https://stackoverflow.com/questions/47871671/difference-between-stddecay-and-stdremove-reference
+* std::decay will remove any cv-qualifer, remove_reference won't. 
+* It will just strip the "reference" part of the type.
+* Therefore std::decay will perform way more type conversions than std::remove_reference.
+*There are also further type modifiers for more nuanced applications that will only perform selected 
+*parts of the set of possible transformations decay does, like remove_cv, remove_volatile or, 
+*in C++20, remove_cvref.
+*/
+
+// NOTE: WE CANNOT SERIALIZE POINTERS
+class Serializer{
+public:
+    template<typename ... Args>
+    Message serialize(const std::string& functionName, Args&&... args){
+        size_t size = functionName.size() + (sizeof(Args) + ...);
+        Message data{size};
+        (encode<Args>(data,args),...);
+        return data;
+    }
+
+    Serializer();
+
+private:
+};
---
+++ src/Codec/Serializer.cpp
@@ -0,0 +1,8 @@
+#include "Codec/Serializer.hpp"
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
+Serializer::Serializer(){
+
+}
