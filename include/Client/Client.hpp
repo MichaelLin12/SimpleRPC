7d1f443612ff37692d138721a51aff74e87fcf14
Michael Lin <michael.lin2032@gmail.com>
2025-12-20 23:18:48 -0500

    add send and receive calls

--- include/Client/Client.hpp
+++ include/Client/Client.hpp
@@ -6,9 +6,12 @@
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
 
+//https://beej.us/guide/bgnet/html/split/index.html
+// Only support IPV4
+// Only support integral types and strings(kinda ... only for function name (need a better way for the general case))
 class Client{
 public:
-    Client();
+    Client(const std::string addr);
 
 
     // Function templates (including template member functions) must be defined 
@@ -18,25 +21,46 @@ public:
 
         //1.Serialize the data
         Message data = serializer.serialize<Args...>(this->id,functionName,std::forward<Args>(args)...);
-        if constexpr (std::is_void_v<Return>) {
-            // 2. Send data over --> expect an OK reply
-            return;
-        } else {
-            // 2. Send data over (expect a result)
-            return result;
-        }
+        send();
+        Return result = receive();
+        return result;
     }
+
+    ~Client();
 
     Client(const Client& other) = delete;
     Client(Client&& other) noexcept;
     Client& operator=(const Client& other) = delete;
     Client& operator=(Client&& other) = delete;
 
+    void snd(const Message& m);
+
+    template<typename Return>
+    Return receive(){
+        char* buf[sizeof(Return)];
+        ssize_t totalRecv = 0;
+        ssize_t bytesRecv = 0;
+
+        while (totalRecv < sizeof(Return)) {
+            bytesRecv = recv(this->soc, buf + totalRecv, sizeof(Return) - totalRecv, 0);
+            if (bytesRecv == 0) {
+                // Connection closed cleanly
+                break;
+            } else if (bytesRecv == -1) {
+                perror("recv");
+                break;
+            }
+            totalRecv += bytesRecv;
+        }
+        //deserialize return
+        return ret;
+    }
+
 private:
     Serializer serializer;
-    boost::uuids::uuid id; // need to be able to pass this down to the message obj
-    //socket for client
-    //maybe a logger obj ... but I don't now ... we want to be 
+    boost::uuids::uuid id;
+    int soc;
+    //logging should be GLOBAL!!!!
     //able to enable/disable it based on the preprocessor/compiler
     //maybe need an object for an exception 
 };
--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -2,10 +2,59 @@
 #include <boost/uuid/uuid.hpp>
 #include <boost/uuid/uuid_generators.hpp>
 #include <boost/uuid/uuid_io.hpp>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
-Client::Client():serializer{},id{}{
+// at some point you should look into scatter and gather io and look at implementing that
+
+Client::Client(const std::string addr):serializer{},id{},soc{0}{
     boost::uuids::random_generator gen;
     this->id = gen();
+
+    int status;
+    struct addrinfo hints;
+    struct addrinfo* res;
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    status = getaddrinfo(addr.c_str(), "3490", &hints, &res);
+    if(status != 0) [[unlikely]] {
+        fprintf(stderr, "gai error: %s\n", gai_strerror(status));
+        exit(1);
+    }
+
+    struct addrinfo* p;
+    for(p = res; p != NULL; p = p->ai_next) {
+        this->soc = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+        if(this->soc == -1) [[unlikely]] {
+            perror("client: socket");
+            continue;
+        }
+
+
+        if (connect(this->soc, p->ai_addr, p->ai_addrlen) == -1) {
+            perror("client: connect");
+            close(this->soc);
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) [[unlikely]] {
+        fprintf(stderr, "client: failed to connect\n");
+        exit(2);
+    }
+
+    freeaddrinfo(res);
+}
+
+Client::~Client(){
+    close(this->soc); 
 }
 
 Client::Client(Client&& other)
@@ -20,4 +69,22 @@ Client& Client::operator=(Client&& other){
     }
 
     return *this;
+}
+
+void Client::snd(const Message& m){
+
+    ssize_t bytes = 0;
+    size_t totalSent = 0;
+    size_t size = m.getSize();
+    char* buffer = m.getBuffer();
+    while(totalSent < static_cast<ssize_t>(size)){ // could use do while loop
+        bytes = send(this->soc, buffer + totalSent, size - bytes, 0);
+        if(bytes == -1){
+            perror("send");
+            break;
+        }
+        totalSent += bytes;
+    }
+
+    return;
 }
