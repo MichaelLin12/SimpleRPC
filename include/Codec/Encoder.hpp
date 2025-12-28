80240c493cea5c8ec04dbe377ec79d85f8fbbd88
Michael Lin <michael.lin2032@gmail.com>
2025-12-28 18:17:04 -0500

    update encoder

--- include/Codec/Encoder.hpp
+++ include/Codec/Encoder.hpp
@@ -5,6 +5,7 @@
 #include <concepts>
 #include <span>
 #include <vector>
+#include "Msg/Message.hpp"
 
 
 class Encoder{
@@ -14,11 +15,16 @@ public:
 
     template<typename T>
     requires (std::same_as<T,std::string>)
-    std::vector<std::byte> encode(T arg){
-        std::size_t sz = arg.size();
-        std::vector<std::byte> res(sizeof(std::size_t) + sz);
-        std::memcpy(res.data(),&(sz),sizeof(std::size_t));
-        std::memcpy(res.data()+sizeof(std::size_t),arg.data(),arg.size());
-        return res;
+    void encode(T arg, Message& msg){
+        encode(arg.size(),msg);
+        msg.addData(arg);
+        return;
+    }
+
+    template<typename T>
+    requires (std::integral<T>)
+    void encode(T arg, Message& msg){
+        // need to deal with big endian and little endian
+        msg.addData(arg);
     }
 };
