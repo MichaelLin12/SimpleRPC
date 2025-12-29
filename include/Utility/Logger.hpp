0c4fdf2061bd66da55327d7c903a3434136cc72b
Michael Lin <michael.lin2032@gmail.com>
2025-12-29 15:38:50 -0500

    added debug info and colors

--- include/Utility/Logger.hpp
+++ include/Utility/Logger.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include "Utility/Colors.hpp"
+
+inline void log_debug(const std::string s){
+    std::cout << BOLDCYAN << s << RESET << std::endl;
+}
 
 inline void log_info(const std::string s){
-    std::cout << s << std::endl;
+    std::cout << BOLDGREEN << s << RESET << std::endl;
 }
 
 inline void log_error(const std::string s){
-    std::cerr << s << std::endl;
+    std::cerr << BOLDRED << s << RESET << std::endl;
 }
