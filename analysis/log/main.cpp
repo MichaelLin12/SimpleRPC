4acd3b78a791ee9a94f339abdf79b867d6ab0f52
Michael Lin <michael.lin2032@gmail.com>
2026-03-21 12:49:25 -0400

    add benchmarking

--- analysis/log/main.cpp
+++ analysis/log/main.cpp
@@ -1,11 +1,16 @@
 #include "log_1.h"
+#include <chrono>
 #include <format>
 
 int main(int argc, char** argv)
 {
+    auto start = std::chrono::steady_clock::now();
     for (int i = 0; i < 10000; i++)
     {
         LOGGING(LogLevel::INFO, "{}", "test");
     }
+    auto end = std::chrono::steady_clock::now();
+    auto elapsed = end - start;
+    std::cout << "Elapsed time: " << elapsed << std::endl;
     return 0;
 }
