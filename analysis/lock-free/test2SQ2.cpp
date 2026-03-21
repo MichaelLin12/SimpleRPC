2fa183e163a82df1d10b081a763a0cdb1fffb7cb
Michael Lin <michael.lin2032@gmail.com>
2026-03-21 19:14:04 -0400

    check the produce/consume of spsc

--- analysis/lock-free/test2SQ2.cpp
+++ analysis/lock-free/test2SQ2.cpp
@@ -1,4 +1,29 @@
 #include "SimpleQ2.hpp"
 #include <iostream>
+#include <thread>
 
-int main() {}
+void produce(SimpleQ<int>& buff)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        buff.offer(i);
+    }
+}
+
+void consume(SimpleQ<int>& buff)
+{
+    int res = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        buff.poll(res);
+        std::cout << res << std::endl;
+    }
+}
+
+int main()
+{
+    SimpleQ<int> buff{10};
+    std::jthread t0{produce, std::ref(buff)};
+    std::jthread t1{consume, std::ref(buff)};
+    return 0;
+}
