5c7c87e60a566181c22d46fabcbb24511c85f86b
Michael Lin <michael.lin2032@gmail.com>
2026-03-25 23:28:41 -0400

    make m const

--- include/Codec/Decoder.hpp
+++ include/Codec/Decoder.hpp
@@ -1,36 +1,39 @@
 #pragma once
+#include "Msg/Message.hpp"
+#include "Utility/Logger.hpp"
 #include <array>
 #include <bit>
+#include <concepts>
 #include <cstddef>
+#include <cstring>
+#include <format>
 #include <span>
 #include <string>
-#include <cstring>
-#include <bit>
-#include <concepts>
-#include "Utility/Logger.hpp"
-#include "Msg/Message.hpp"
-#include <format>
 
-class Decoder{
+class Decoder
+{
 public:
-    Decoder()=default;
-    ~Decoder()=default;
+    Decoder() = default;
+    ~Decoder() = default;
 
-    template<typename T>
-    requires (std::same_as<T,std::string>)
-    T decode(Message& m){
+    template <typename T>
+        requires(std::same_as<T, std::string>)
+    T decode(const Message& m)
+    {
         std::size_t size = decode<size_t>(m);
         auto buffer = m.getData();
         LOGGING(LogLevel::DEBUG, "Size is: {}", size);
         LOGGING(LogLevel::DEBUG, "buffer size is: {}", buffer.size());
-        std::string rt = std::string{reinterpret_cast<const char*>(buffer.data()), size};
+        std::string rt =
+            std::string{reinterpret_cast<const char*>(buffer.data()), size};
         m.setOffset(m.getOffset() + size);
         return rt;
     }
 
-    template<typename T>
-    requires (std::integral<T>)
-    T decode(Message& m){
+    template <typename T>
+        requires(std::integral<T>)
+    T decode(const Message& m)
+    {
         auto buffer = m.getData();
         LOGGING(LogLevel::DEBUG, "buffer size is: {}", buffer.size());
         LOGGING(LogLevel::DEBUG, "argument size is: {}", sizeof(T));
@@ -38,10 +41,12 @@ public:
         std::memcpy(&value, buffer.data(), sizeof(T));
         LOGGING(LogLevel::DEBUG, "value is: {}", value);
         m.setOffset(m.getOffset() + sizeof(T));
-        if constexpr(std::endian::native == std::endian::little){
+        if constexpr (std::endian::native == std::endian::little)
+        {
             value = std::byteswap(value);
         }
-        LOGGING(LogLevel::DEBUG, "value is:(potentially after byteswap) {}", value);
+        LOGGING(LogLevel::DEBUG, "value is:(potentially after byteswap) {}",
+                value);
         return value;
     }
 };
